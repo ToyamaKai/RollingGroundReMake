439d3bc01ad6a7abd42f9d548eca028e51612c20
KaiToyama <toyamakai2002@gmail.com>
2025-09-17 00:34:09 +0900

    処理内容などの記述(実装は後日やる)

---
+++ RollingGroundUnityProject/Assets/BaseSystem/Scripts/CreateSystem/BlockManipulator.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+/// <summary>
+/// ブロックの設置・削除を行う機能の実装
+/// </summary>
+public class BlockManipulator
+{
+    //今はやることを書こう
+    //処理の内容
+    //マウスポインターからレイキャストを飛ばし、指定したY座標に到達した際にX, Z座標の数値を四捨五入し、整数に丸め込む。
+    //Y座標はspaceで+1, Lshift or Lctrlで-1. Planeも連動して上下する。
+    //あと設置場所を見やすいように半透明でブロックをおす
+}
