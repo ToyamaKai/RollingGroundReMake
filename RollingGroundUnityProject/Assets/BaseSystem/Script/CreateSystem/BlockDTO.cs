b63d2752da3eddc0319f39da9d08fdffbfbf2f94
ToyamaKai <kai_toyama@digitalfactor.co.jp>
2026-01-08 19:53:04 +0900

    DTOのサンプルを作成

---
+++ RollingGroundUnityProject/Assets/BaseSystem/Script/CreateSystem/BlockDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ブロックの(DTO)データ転送オブジェクト
+/// </summary>
+[Serializable]
+public class BlockDTO
+{
+    public int id;
+    public int x, y, z;
+    public int rot;
+    public ushort flags;
+    public Dictionary<string, string> meta;
+}
