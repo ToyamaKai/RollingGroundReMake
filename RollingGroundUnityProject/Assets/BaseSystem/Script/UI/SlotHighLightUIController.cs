86785f246553ed52abbb68f3cbab817c6f795972
ToyamaKai <kai_toyama@digitalfactor.co.jp>
2026-03-02 17:59:59 +0900

    Slotのハイライトロジックの追加(InputSystemからのイベント登録に修正必要)

---
+++ RollingGroundUnityProject/Assets/BaseSystem/Script/UI/SlotHighLightUIController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+public class SlotHighLightUIController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_highLightSlot;
+    private BlockHotbar m_blockHotbar;
+    private const float k_slotSpace = 160.0f;
+    private const float k_firstSlotPositionX = -640.0f;
+
+    private void Awake()
+    {
+        m_blockHotbar = GameObject.FindFirstObjectByType<BlockHotbar>();
+
+        Vector3 UIposition = m_highLightSlot.transform.position;
+        UIposition= new Vector3(k_firstSlotPositionX, 0, 0);
+        m_highLightSlot.transform.localPosition = UIposition;
+    }
+
+    private void Update()
+    {
+        MoveHightLightUI();
+    }
+
+    private void MoveHightLightUI()
+    {
+        float position = k_firstSlotPositionX + (k_slotSpace * m_blockHotbar.GetSelectedSlot());
+        Vector3 UIposition = m_highLightSlot.transform.localPosition;
+        UIposition.x = position;
+
+        m_highLightSlot.transform.localPosition = UIposition;
+    }
+}
