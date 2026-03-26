64c8cbd4705573483f638b049a3f046d2d4ae9f5
ToyamaKai <kai_toyama@digitalfactor.co.jp>
2026-03-26 19:04:51 +0900

    ステージ情報の作成と、回転状態の計算処理部分の作成

--- RollingGroundUnityProject/Assets/BaseSystem/Script/Gimmick/StageRotation.cs
+++ RollingGroundUnityProject/Assets/BaseSystem/Script/Gimmick/StageRotation.cs
@@ -1,5 +1,39 @@
-﻿
+﻿using RollingGround;
 
-public class StageRotation 
+namespace RollingGround
 {
+    /// <summary>
+    /// ステージ回転の実処理
+    /// </summary>
+    public static class StageRotation
+    {
+        public enum RotationAxis { X, Y, Z }; //回転方向を指定する際に使用
+
+        /// <summary>
+        /// 入力を受け取り、StageDataを更新するロジック
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="axis"></param>
+        /// <param name="direction"></param>
+        public static void RxecuteRotate(StageData data, RotationAxis axis, int direction)
+        {
+            switch (axis)
+            {
+                case RotationAxis.X: data.StepX = ClamStep(data.StepX + direction); break;
+                case RotationAxis.Y: data.StepY = ClamStep(data.StepY + direction); break;
+                case RotationAxis.Z: data.StepZ = ClamStep(data.StepZ + direction); break;
+            }
+        }
+
+        /// <summary>
+        ///  0~3の範囲に丸め込む
+        /// </summary>
+        /// <param name="step"></param>
+        /// <returns></returns>
+        private static int ClamStep(int step)
+        {
+            return (step % 4 + 4) % 4;
+        }
+
+    }
 }
