140b6e452e3d3c125d948ab455658b6d252bb8ae
ToyamaKai <toyamakai2002@gmail.com>
2026-05-21 19:00:02 +0900

    リフトブロックを実装するにあたり以下の実装 ・動作タイプ(常時、プレイヤー接触時、スイッチ式) ・実装部の値の受け渡し ・実処理部の値の受け渡し

---
+++ RollingGroundUnityProject/Assets/BaseSystem/Script/Gimmick/LiftBlock.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// リフトブロック
+/// </summary>
+public class LiftBlock
+{
+    private Vector3 m_moveDirection = new Vector3(0, 0, 0);   //移動方向
+    private float m_moveDistance = 0f;           //移動距離
+    private float m_moveSpeed = 0f;              //移動速度
+    private LiftTriggerType m_liftTriggerType = LiftTriggerType.Always; //リフトのトリガータイプ
+
+    public LiftBlock(Vector3 moveDirection, float moveDistance, float moveSpeed, LiftTriggerType liftTriggerType) : base()
+    {
+        m_moveDirection = moveDirection;
+        m_moveDistance = moveDistance;
+        m_moveSpeed = moveSpeed;
+        m_liftTriggerType = liftTriggerType;
+    }
+
+    //TODO: DOTWeen等を用いたリフトブロックの動く処理の記述
+}
---
+++ RollingGroundUnityProject/Assets/BaseSystem/Script/Gimmick/MLiftBlock.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class MLiftBlock : MonoBehaviour
+{
+    [Header("移動設定")]
+    [SerializeField]
+    private Vector3 m_moveDirection = Vector3.up; //移動方向
+
+    [SerializeField]
+    private float m_moveDistance = 3.0f; //移動距離
+
+    [SerializeField]
+    private float m_moveSpeed = 2.0f; //移動速度
+
+    [Header("トリガー設定")]
+    [SerializeField]
+    private LiftTriggerType m_liftTriggerType = LiftTriggerType.Always; //リフトのトリガータイプ
+
+    private LiftBlock m_liftBlock;
+
+    private void Awake()
+    {
+        m_liftBlock = new LiftBlock(m_moveDirection, m_moveDistance, m_moveSpeed, m_liftTriggerType);
+    }
+
+    //TODO: LiftBlockに記述したリフトブロックの動く処理を呼び出す処理の記述
+}
