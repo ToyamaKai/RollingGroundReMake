eda7b245aa078001b7272af8f2375ab18f0e1fea
ToyamaKai <toyamakai2002@gmail.com>
2026-06-04 19:41:32 +0900

    ・ブロック設置の処理の変更 ・ブロックデータベースの作成

---
+++ RollingGroundUnityProject/Assets/BaseSystem/Script/System/Block/MBlockDatabase.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// ブロックのデータベースを管理するクラス
+/// </summary>
+public class MBlockDatabase : MonoBehaviour
+{
+    [SerializeField]
+    private BlockData[] blockDatas;
+
+    public BlockData GetBlockData(BlockID id)
+    {
+        foreach (var blockData in blockDatas)
+        {
+            if (blockData.id == id)
+            {
+                return blockData;
+            }
+        }
+        return null;
+    }
+}
---
+++ RollingGroundUnityProject/Assets/BaseSystem/Script/System/StageImport/MStageBuilder.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// ステージの構築を行うクラス
+/// </summary>
+public class MStageBuilder : MonoBehaviour
+{
+    MBlockDatabase m_blockDatabase; // ブロックのデータベース参照
+    BlockManipulator m_blockManipulator; // ブロックの設置・削除を行うクラス参照
+
+    private void Awake()
+    {
+        m_blockDatabase = GameObject.FindFirstObjectByType<MBlockDatabase>();
+        m_blockManipulator = GameObject.FindFirstObjectByType<BlockManipulator>();
+    }
+
+
+    public void BuildStage()
+    {
+
+    }
+}
