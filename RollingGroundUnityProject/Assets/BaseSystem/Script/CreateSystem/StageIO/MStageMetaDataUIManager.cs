2cd29634ea19b27b4e5fda2abe9e382fbd421178
ToyamaKai <toyamakai2002@gmail.com>
2026-06-10 18:07:54 +0900

    仮のScriptとUIを作成

--- RollingGroundUnityProject/Assets/BaseSystem/Script/CreateSystem/StageIO/MStageMetaDataUIManager.cs
+++ RollingGroundUnityProject/Assets/BaseSystem/Script/CreateSystem/StageIO/MStageMetaDataUIManager.cs
@@ -15,6 +15,17 @@ public class MStageMetaDataUIManager : MonoBehaviour
 
     public void SetStageName()
     {
+        m_stageName = m_stageNameInputFiel.text;
+    }
 
+    public void SetComment()
+    {
+        m_comment = m_commentInputField.text;
+    }
+
+    public void CreateStageMetaData()
+    {
+        StageMetaData stageMetaData = m_factory.CreateStageMetaData(m_stageName, "Unknown", "1.0.0", 1, m_comment);
+        Debug.Log($"Stage Meta Data Created: {stageMetaData.StageName}, {stageMetaData.Comment}");
     }
 }
