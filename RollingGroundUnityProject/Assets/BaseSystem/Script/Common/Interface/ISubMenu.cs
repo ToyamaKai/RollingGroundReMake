c854e5e17e58cbe77bcf2126c27c52d43c6326e3
ToyamaKai <toyamakai2002@gmail.com>
2026-06-24 19:02:09 +0900

    メニューにおけるサブメニュー選択を切り替えを行う処理のガワを追加(処理未記述)

---
+++ RollingGroundUnityProject/Assets/BaseSystem/Script/Common/Interface/ISubMenu.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public interface ISubMenu
+{
+    void OpenSubMenu();
+    void CloseSubMenu();
+}
---
+++ RollingGroundUnityProject/Assets/BaseSystem/Script/CreativeMode/UI/CreativeModeMenu.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreativeModeMenu : MonoBehaviour
+{
+    [SerializeField]
+    private List<MonoBehaviour> m_subMenus;
+
+    private List<ISubMenu> m_menus = new();
+
+    private ISubMenu m_currentMenu;
+
+    private int m_currentMenuIndex = 0;
+
+    private void Awake()
+    {
+        foreach(var menu in m_subMenus)
+        {
+            if(menu is ISubMenu subMenu)
+            {
+                m_menus.Add(subMenu);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 入力に応じてメニュー選択を切り替える(1の増減)
+    /// </summary>
+    public void OnMoveMenu()
+    {
+
+    }
+}
