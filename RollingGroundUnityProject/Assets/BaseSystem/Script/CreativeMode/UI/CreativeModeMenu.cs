60862648ca635dce6efc5f27335bde0f5f1b0336
KaiToyama <toyamakai2002@gmail.com>
2026-06-29 00:06:53 +0900

    メニュー・サブメニューの開閉処理の記述(メニューを開く処理は未記述)

--- RollingGroundUnityProject/Assets/BaseSystem/Script/CreativeMode/UI/CreativeModeMenu.cs
+++ RollingGroundUnityProject/Assets/BaseSystem/Script/CreativeMode/UI/CreativeModeMenu.cs
@@ -7,11 +7,13 @@ using UnityEngine.InputSystem;
 public class CreativeModeMenu : IInputReceiver
 {
     private MGameInputManager m_gameInputManager;
+    private GameObject m_menuGameObject;
     private List<MonoBehaviour> m_subMenus;
     private List<ISubMenu> m_menus = new();
     private ISubMenu m_currentMenu;
     private int m_currentMenuIndex = 0;
     private int m_menuCount = 0;
+    private bool m_isSubMenuOpen;
 
     public CreativeModeMenu(MGameInputManager gameInputManager, List<MonoBehaviour> subMenus)
     {
@@ -26,8 +28,50 @@ public class CreativeModeMenu : IInputReceiver
                 m_menus.Add(subMenu);
             }
         }
-        m_menuCount = m_menus.Count;
 
+        m_menuCount = m_menus.Count;
+    }
+
+    #region 入力処理関連
+    /// <summary>
+    /// メニューとサブメニューを閉じる処理
+    /// </summary>
+    /// <param name="context"></param>
+    public void OnCloseMenu(InputAction.CallbackContext context)
+    {
+        if (!context.performed) return;
+
+        if (m_isSubMenuOpen)
+        {
+            m_menus[m_currentMenuIndex].OpenSubMenu();
+            m_isSubMenuOpen = false;
+        }
+
+        m_menuGameObject.SetActive(false);
+    }
+
+    /// <summary>
+    /// サブメニューを開く処理
+    /// </summary>
+    /// <param name="context"></param>
+    public void OnOpenSubMenu(InputAction.CallbackContext context)
+    {
+        if (!context.performed || m_isSubMenuOpen) return;
+
+        m_isSubMenuOpen = true;
+        m_menus[m_currentMenuIndex].OpenSubMenu();
+    }
+
+    /// <summary>
+    /// サブメニューのみを閉じる処理
+    /// </summary>
+    /// <param name="context"></param>
+    public void OnCloseSubMenu(InputAction.CallbackContext context)
+    {
+        if (!context.performed || !m_isSubMenuOpen) return;
+
+        m_menus[m_currentMenuIndex].OpenSubMenu();
+        m_isSubMenuOpen = false;
     }
 
     /// <summary>
@@ -40,4 +84,5 @@ public class CreativeModeMenu : IInputReceiver
 
         m_currentMenuIndex = ((int)(m_currentMenuIndex + direction) + m_menuCount) % m_menuCount;
     }
+#endregion
 }
