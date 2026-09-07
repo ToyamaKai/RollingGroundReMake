aa744572f2483f6bbd7e8d1392de89283479d65f
KaiToyama <toyamakai2002@gmail.com>
2026-09-08 04:58:19 +0900

    デバッグ用タイトルシーンの追加

---
+++ RollingGroundUnityProject/Assets/BaseSystem/Script/Common/UI/MDebugTitleController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MDebugTitleController : MonoBehaviour
+{
+    /// <summary>
+    /// デバッグステージのロード
+    /// </summary>
+    public void LoadDebugStage()
+    {
+        SceneManager.LoadScene("DebugStage01");
+    }
+}
---
+++ RollingGroundUnityProject/Assets/FirstSeriesSystem/Script/Common/TitleController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleController : MonoBehaviour
+{
+    /// <summary>
+    /// 新規ゲーム開始ボタンが押されたときに呼ばれるメソッド
+    /// </summary>
+    public void LoadNewGame()
+    {
+        SceneManager.LoadScene("");
+    }
+
+    public void LoadCreativeMode()
+    {
+
+    }
+}
