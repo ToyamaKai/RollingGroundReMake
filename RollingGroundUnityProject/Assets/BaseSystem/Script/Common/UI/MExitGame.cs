93e80368b71dc03ee4de5f157a0e4771efdd60ab
KaiToyama <toyamakai2002@gmail.com>
2026-09-08 23:11:37 +0900

    ゲーム終了処理, ボタンの追加

---
+++ RollingGroundUnityProject/Assets/BaseSystem/Script/Common/UI/MExitGame.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class MExitGame : MonoBehaviour
+{
+
+    /// <summary>
+    /// ゲーム終了処理
+    /// </summary>
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
